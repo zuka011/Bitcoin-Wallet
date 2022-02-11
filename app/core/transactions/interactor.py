f3c569e8b5c84d613dff50a27b30a20d52985d4e
zuka011 <zmuji18@freeuni.edu.ge>
2022-02-11 16:22:59 +0400

    Refactor TransactionInteractor

--- app/core/transactions/interactor.py
+++ app/core/transactions/interactor.py
@@ -39,46 +39,65 @@ class TransactionInteractor:
                 f"Cannot transfer funds from {source_address} with incorrect API key."
             )
 
+        transfer_fee = self.__get_transfer_fee(
+            source_address=source_address,
+            destination_address=destination_address,
+            api_key=api_key,
+        )
+
+        self.__withdraw_amount(wallet_address=source_address, amount_btc=amount_btc)
+        self.__deposit_amount(
+            wallet_address=destination_address,
+            amount_btc=amount_btc,
+            transfer_fee=transfer_fee,
+        )
+
+    def __get_transfer_fee(
+        self, *, source_address: str, destination_address: str, api_key: str
+    ) -> float:
+        """Returns the transfer fee for transferring to the specified wallet address."""
         if self.__wallet_repository.is_wallet_owner(
             wallet_address=destination_address, api_key=api_key
         ):
-            transfer_fee = (
-                self.__system_configuration.get_same_user_transfer_fee_percentage()
-            )
+            return self.__system_configuration.get_same_user_transfer_fee_percentage()
         else:
-            transfer_fee = (
-                self.__system_configuration.get_cross_user_transfer_fee_percentage()
-            )
+            return self.__system_configuration.get_cross_user_transfer_fee_percentage()
 
-        source_wallet = self.__wallet_repository.get_wallet(
-            wallet_address=source_address
-        )
-        destination_wallet = self.__wallet_repository.get_wallet(
-            wallet_address=destination_address
-        )
+    def __withdraw_amount(self, wallet_address: str, *, amount_btc: float) -> None:
+        """Withdraws the specified amount from the given wallet."""
+        wallet = self.__get_wallet(wallet_address)
+        updated_balance_btc = wallet.balance_btc - amount_btc
 
-        updated_source_balance_btc = source_wallet.balance_btc - amount_btc
-        updated_destination_balance_btc = (
-            destination_wallet.balance_btc + deduct_percentage(amount_btc, transfer_fee)
+        self.__update_wallet_balance(
+            wallet_address=wallet_address, balance_btc=updated_balance_btc
         )
 
+    def __deposit_amount(
+        self, wallet_address: str, *, amount_btc: float, transfer_fee: float
+    ) -> None:
+        """Deposits the specified amount (after deducting transfer fee) to the given wallet."""
+        wallet = self.__get_wallet(wallet_address)
+        updated_balance_btc = wallet.balance_btc + deduct_percentage(
+            amount_btc, transfer_fee
+        )
+
+        self.__update_wallet_balance(
+            wallet_address=wallet_address, balance_btc=updated_balance_btc
+        )
+
+    def __get_wallet(self, wallet_address: str) -> Wallet:
+        """Returns the wallet with the specified address."""
+        return self.__wallet_repository.get_wallet(wallet_address=wallet_address)
+
+    def __update_wallet_balance(
+        self, wallet_address: str, *, balance_btc: float
+    ) -> None:
+        """Updates the balance of the specified wallet."""
         self.__wallet_repository.update_wallet(
             Wallet(
-                address=source_address,
-                balance_btc=updated_source_balance_btc,
-                balance_usd=self.__currency_converter.to_usd(
-                    updated_source_balance_btc
-                ),
+                address=wallet_address,
+                balance_btc=balance_btc,
+                balance_usd=self.__currency_converter.to_usd(balance_btc),
             ),
-            wallet_address=source_address,
-        )
-        self.__wallet_repository.update_wallet(
-            Wallet(
-                address=destination_address,
-                balance_btc=updated_destination_balance_btc,
-                balance_usd=self.__currency_converter.to_usd(
-                    updated_destination_balance_btc
-                ),
-            ),
-            wallet_address=destination_address,
+            wallet_address=wallet_address,
         )
